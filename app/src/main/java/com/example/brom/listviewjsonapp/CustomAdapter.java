6ecea4f74cfcf0579f9d81b3641c0807cf89dc38
a17pongo <cEh7Yuf4>
2018-04-26 14:53:35 +0200

    started working on custom arrayAdapter

---
+++ app/src/main/java/com/example/brom/listviewjsonapp/CustomAdapter.java
@@ -0,0 +1,13 @@
+package com.example.brom.listviewjsonapp;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by a17pongo on 2018-04-26.
+ */
+
+public class CustomAdapter extends ArrayAdapter<MountainDetails>{
+
+
+
+}
